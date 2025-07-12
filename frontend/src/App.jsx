18714f464237503f3638e9b1a149ba4d5518ba1f
Madhesh K <madheshk7239087@gmail.com>
2025-07-12 16:20:05 +0530

    Add side navbar for Resume Builder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,18 +5,22 @@ import Navbar from './Nav/Navbar'
 import UploadResume from './pages/UploadResume'
 import ImproveResume from './pages/ImproveResume'
 import ResumeScore from './pages/ResumeScore'
+import ResumeBuilderNavBar from './Resume Builder Nav/ResumeBuilderNavBar'
 
 const App = () => {
   return(
     <>
       <Routes>
-        <Route path="/" element={<LandingPage />}></Route>
-            <Route path="/app" element={<Navbar/>}>
-               <Route path="upload" element={<UploadResume/>} />
-                <Route path="score" element={<ResumeScore/>} />
-                 <Route path="improve" element={<ImproveResume/>} />
-         
-         
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/app" element={<Navbar/>}>
+          <Route path="upload" element={<UploadResume/>} />
+          <Route path="score" element={<ResumeScore/>} />
+          <Route path="improve" element={<ImproveResume/>} />
+        </Route>
+        
+        
+        <Route path="/builder" element={<ResumeBuilderNavBar/>}>
+    
         </Route>
       </Routes>
     </>
