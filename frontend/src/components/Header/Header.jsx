5c58e877e05f63314b53133e5dabd82710298154
Madhesh K <madheshk7239087@gmail.com>
2025-07-13 17:26:08 +0530

    taken the signup page outside of the /app route

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useNavigate } from 'react-router'
 
 const Header = () => {
+  const navigate = useNavigate();
   return (
     <div className="flex justify-between items-center px-10 py-2.5 bg-[#FFF2E0] border-b-[3px] border-[#c2b2d4] ">
       <div className="flex items-center font-bold text-[18px] text-[#898AC4]">
@@ -13,7 +15,7 @@ const Header = () => {
           <li className="text-[14px] text-[#898AC4] hover:text-[#7d63a9] cursor-pointer font-medium">Home</li>
           <li className="text-[14px] text-[#898AC4] hover:text-[#7d63a9] cursor-pointer font-medium">Features</li>
           <li className="text-[14px] text-[#898AC4] hover:text-[#7d63a9] cursor-pointer font-medium">Login</li>
-          <li className="text-[14px] text-[#898AC4] hover:text-[#7d63a9] cursor-pointer font-medium">Sign Up</li>
+          <li className="text-[14px] text-[#898AC4] hover:text-[#7d63a9] cursor-pointer font-medium" onClick={() => navigate('/signup')}>Sign Up</li>
         </ul>
       </div>
     </div>
