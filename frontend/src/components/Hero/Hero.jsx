f4ea81a3beec0ba68ff966c9d826a72767967a85
Madhesh K <madheshk7239087@gmail.com>
2025-07-13 21:25:32 +0530

    theme upadate for landing page

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -3,22 +3,33 @@ import heroImage from '../../assets/hero-image.png'
 import { useNavigate } from 'react-router'
 
 const Hero = () => {
-  const navigate=useNavigate();
+  const navigate = useNavigate();
+  
   return (
-    <div className="flex justify-between items-center px-[80px] py-[60px] bg-[#fff6ef] gap-[40px]">
+    <div className="flex justify-between items-center px-[80px] py-[60px] bg-[#F9FAFB] gap-[40px]">
       <div className="flex-1 max-w-[55%]">
         <div className="mb-[25px]">
-          <h1 className="text-[38px] font-bold text-[#5c4d7d] mb-[15px]">
+          <h1 className="text-[38px] font-bold text-[#1F2937] mb-[15px]">
             Optimize Your Resume for ATS Success
           </h1>
-          <p className="text-[16px] text-[#5c4d7d] leading-[1.6]">
+          <p className="text-[16px] text-[#6B7280] leading-[1.6]">
             Get your resume past the Applicant Tracking Systems and into the hands of hiring managers. Our AI-powered tool analyzes and improves your resume for maximum visibility
           </p>
         </div>
 
         <div className="flex gap-[20px]">
-          <button className="bg-[#dcd9f4] text-[#5c4d7d] px-5 py-3 rounded-md hover:bg-[#c2bbf0] transition-all duration-300" onClick={()=>navigate('/app')} > Upload Your Resume </button>
-          <button className="bg-[#b9b4e2]  text-white px-5 py-3 rounded-md  hover:bg-[#a59ce0] transition-all duration-300" onClick={() => navigate('/builder')}> Build ATS Ready Resume </button>
+          <button 
+            className="bg-[#6366F1] text-white px-5 py-3 rounded-md hover:bg-[#4F46E5] transition-all duration-300" 
+            onClick={() => navigate('/app')}
+          > 
+            Upload Your Resume 
+          </button>
+          <button 
+            className="bg-[#0EA5E9] text-white px-5 py-3 rounded-md hover:bg-[#0284C7] transition-all duration-300"
+            onClick={() => navigate('/builder')}
+          > 
+            Build ATS Ready Resume 
+          </button>
         </div>
       </div>
 
